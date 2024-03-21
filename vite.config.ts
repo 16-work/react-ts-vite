d80b6dbc1f7b8bee784f2d6369a632970c603b44
16 <soup16-work@outlook.com>
2024-03-21 14:57:09 +0800

    config: alias @

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import react from '@vitejs/plugin-react-swc';
+import { alias } from './config/vite/alias';
 
 export default function () {
     return {
         plugins: [react()],
+        resolve: { alias },
     };
 }
