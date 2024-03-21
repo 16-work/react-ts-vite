d49669762a22ffe9eaf1a1dcd91b0749965b9b5d
16 <soup16-work@outlook.com>
2024-03-21 17:54:17 +0800

    feat: NoData.tsx

---
+++ src/component/base/NoData.tsx
@@ -0,0 +1,7 @@
+import ImgNoData from '@/asset/img/no-data.png';
+
+export const NoData = () => {
+    // 需要父元素有relative属性才能居中
+
+    return <img src={ImgNoData} className="position-center absolute w-200" />;
+};
