09e0ba37a65b8ef4ff2e748e3b48ebe0dd24f8e3
16 <soup16-work@outlook.com>
2024-03-21 15:11:24 +0800

    config: vite env

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import react from '@vitejs/plugin-react-swc';
 import { alias } from './config/vite/alias';
+import { ConfigEnv, loadEnv } from 'vite';
+import path from 'path';
+import { parseEnv } from './config/vite/utils';
 
-export default function () {
+export default ({ mode }: ConfigEnv) => {
+    const root = path.resolve(__dirname, './config/env');
+    const env = parseEnv(loadEnv(mode, root));
+
     return {
         plugins: [react()],
         resolve: { alias },
+        envDir: './config/env', // 读取环境变量目录
     };
-}
+};
