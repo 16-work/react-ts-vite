7ce1ce31bd1ce52ce3f2a11eb9612a4ed8c0e867
16 <soup16-work@outlook.com>
2024-03-21 16:08:38 +0800

    feat: base styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@/asset/theme/index.css';
+import '@/asset/css/base.scss';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
