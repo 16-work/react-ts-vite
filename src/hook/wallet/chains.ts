1e2fb9ff28ac541512ab89a58c19f0aa03d3e1e6
16 <soup16-work@outlook.com>
2024-03-25 15:33:45 +0800

    feat: switch chain

--- src/hook/wallet/chains.ts
+++ src/hook/wallet/chains.ts
@@ -3,7 +3,16 @@ export const defaultChains = [
         chainId: 1,
         name: 'Ethereum',
         currency: 'ETH',
-        explorerUrl: 'https://etherscan.io',
+        precision: 18,
         rpcUrl: 'https://cloudflare-eth.com',
+        explorerUrl: 'https://etherscan.io',
+    },
+    {
+        chainId: 137,
+        name: 'Polygon',
+        currency: 'MATIC',
+        precision: 18,
+        rpcUrl: 'https://polygon.llamarpc.com',
+        explorerUrl: 'https://polygonscan.com/',
     },
 ];
