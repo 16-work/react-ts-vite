2b34bc4532de00bde884a5263fce66e6edc67f49
16 <soup16-work@outlook.com>
2024-03-26 15:33:48 +0800

    feat: BoxScrollLoad.tsx

---
+++ src/component/box/BoxScrollLoad.tsx
@@ -0,0 +1,23 @@
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { Divider } from 'antd';
+
+export const BoxScrollLoad = (props: { id: string; list: any[]; hasMore: boolean; onLoadMore: () => void; children: React.ReactNode; className?: string }) => {
+    return (
+        <div id={props.id} className={`w-full h-full overflow-auto ${props.className}`}>
+            <InfiniteScroll
+                scrollableTarget={props.id}
+                dataLength={props.list.length}
+                next={props.onLoadMore}
+                hasMore={props.hasMore}
+                loader={
+                    <div className="relative w-full h-50">
+                        <Load />
+                    </div>
+                }
+                endMessage={<Divider className="!text-18 !text-tip">It is all, nothing more.</Divider>}
+            >
+                {props.children}
+            </InfiniteScroll>
+        </div>
+    );
+};
