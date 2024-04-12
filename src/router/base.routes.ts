f463db29732a167cfdbbb1f9d2d1627a51ac0a63
16 <soup16-work@outlook.com>
2024-04-12 15:15:14 +0800

    perf: routes files

--- src/router/base.routes.ts
+++ src/router/base.routes.ts
@@ -1,3 +1,10 @@
+import { PageHome } from '@/view/home';
 import { RouteObject } from 'react-router-dom';
 
-export const baseRoutes: RouteObject[] = [];
+export const baseRoutes: RouteObject[] = [
+    // 首页
+    {
+        index: true,
+        Component: PageHome,
+    },
+];
