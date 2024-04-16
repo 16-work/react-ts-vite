0e8c8acbe00dd31ed204f3a31b05673579abee5c
16 <soup16-work@outlook.com>
2024-04-16 15:03:24 +0800

    fix: useRequest

--- src/common/util/ahooks/useRequest.ts
+++ src/common/util/ahooks/useRequest.ts
@@ -1,31 +1,21 @@
 import * as hooks from 'ahooks';
-import { Options, Service, Plugin, Result } from 'ahooks/lib/useRequest/src/types';
+import { Options, Service, Result } from 'ahooks/lib/useRequest/src/types';
 
 /* loading -> isLoading & 调整onError位置 */
 export function newUseRequest<TData, TParams extends any[]>(
     service: Service<TData, TParams>,
-    onErrorOrOptions?: ((e: Error, params: any) => void) | Options<TData, TParams>,
-    optionsOrOnError?: Options<TData, TParams> | ((e: Error, params: any) => void),
-    plugins?: Plugin<TData, TParams>[]
+    onError: Options<TData, TParams> | ((e: Error, params: any) => void) = {},
+    options: Options<TData, TParams> = {}
 ): Omit<Result<TData, TParams> & { isLoading: boolean }, 'loading'> {
-    let options: Options<TData, TParams> | undefined;
-    let onError: ((e: Error, params: any) => void) | undefined;
-
-    if (typeof optionsOrOnError === 'function') {
-        onError = optionsOrOnError;
-        options = onErrorOrOptions as Options<TData, TParams>;
-    } else {
-        options = optionsOrOnError;
-        onError = onErrorOrOptions as (e: Error, params: any) => void;
-    }
-
     if (typeof onError === 'function') {
         options = {
             ...options,
             onError,
         };
+    } else {
+        options = onError;
     }
 
-    const { loading, ...res } = hooks.useRequest(service, options, plugins);
+    const { loading, ...res } = hooks.useRequest(service, options);
     return { ...res, isLoading: loading };
 }
