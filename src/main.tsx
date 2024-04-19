a1cea8905aa14d9fa6d000e311d307b33d7aed63
16 <soup16-work@outlook.com>
2024-04-19 18:33:58 +0800

    chore: close React.StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@/asset/theme/index.css';
 import '@/asset/css/base.scss';
@@ -9,10 +8,8 @@ import { browserRouter } from './router/index.ts';
 import 'virtual:svg-icons-register';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <div className="h-full">
-            <RouterProvider router={browserRouter} />
-            <ToastContainer position="top-center" autoClose={2000} />
-        </div>
-    </React.StrictMode>
+    <div className="h-full">
+        <RouterProvider router={browserRouter} />
+        <ToastContainer position="top-center" autoClose={2000} />
+    </div>
 );
