7d6869c47704ce9802112fd5ec4110cc24457111
16 <soup16-work@outlook.com>
2024-05-09 18:52:37 +0800

    feat: format address & token

--- src/common/util/format.ts
+++ src/common/util/format.ts
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import { formatUnits, parseUnits } from 'viem';
 
 export const format = {
     time: (time: Date | number, format: string = 'YYYY/MM/DD HH:mm:ss') => {
@@ -16,5 +17,37 @@ export const format = {
         else if (seconds <= 60 * 60 * 24 * 2) return 'a day ago';
         else if (seconds <= 60 * 60 * 24 * 31) return Math.floor(seconds / 60 / 60 / 24) + ' day ago';
         else return moment(time).format('YYYY/MM/DD HH:mm:ss');
+    },
+
+    address: (str: string, first: number, last: number) => {
+        if (str && typeof str != 'string') str = str + '';
+        if (!str || str.length <= last + first) return str;
+
+        return str.slice(0, first) + '...' + str.slice(str.length - last, str.length);
+    },
+
+    token: {
+        unit: {
+            /** 转为原始值: eg. 1 ether -> 1000000000000000000 wei */
+            origin: (value: string, decimals: number = 18) => {
+                return parseUnits(value, decimals);
+            },
+            /** 转为显示值: eg. 1000000000000000000 wei -> 1 ether */
+            display: formatUnits,
+        },
+
+        price: {
+            /** 将原始价格转为常规价格: eg. 1500000000000000000 wei -> 1.50 ether */
+            common: (num: bigint, format: string = '0,0.00', decimals: number = 18) => {
+                const str = formatUnits(num, decimals);
+                return numeral(str).format(format);
+            },
+            /** 将原始价格转为美元价格 */
+            usdt: (usdtUnitPrice: string, value: bigint, format: string = '0,0.00') => {
+                const usdtValue = (value * BigInt(Math.round(Number(usdtUnitPrice) * 10000))) / 10000n;
+                const final = formatUnits(usdtValue, 18);
+                return numeral(final).format(format);
+            },
+        },
     },
 };
