ae085a09e9fd1181b5adb32e9c05d41b390ebb51
16 <soup16-work@outlook.com>
2024-05-27 19:00:40 +0800

    feat: TokenIcon.tsx

---
+++ src/component/web3/TokenIcon.tsx
@@ -0,0 +1,14 @@
+import { Image } from 'antd';
+import IconDefaultToken from '@/asset/img/token-default.png';
+
+export const TokenIcon = (props: { size: number; icon: string; className?: string }) => {
+    const pxToRem = (px: number) => {
+        return `${px / 16}rem`;
+    };
+
+    return (
+        <span className={`flex-align-x ${props.className}`}>
+            <Image src={props.icon} fallback={IconDefaultToken} width={pxToRem(props.size)} height={pxToRem(props.size)} preview={false} />
+        </span>
+    );
+};
